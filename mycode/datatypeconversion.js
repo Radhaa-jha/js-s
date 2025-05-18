4089c4e47d258d5a8abe59367bf059bddbe12298
Radhaa-jha <jharadha7869@gmail.com>
2025-05-18 11:01:50 +0000

    varible datatype conversion

---
+++ mycode/datatypeconversion.js
@@ -0,0 +1,3 @@
+let num="radha122"
+let num1=Number(num)
+console.log(num1)
---
+++ mycode/varibles.js
@@ -0,0 +1,10 @@
+const accountid=12345
+let accemail="jhar15@gmail.com"
+let accno=258963
+let city="gwalior"
+//prefer not use of var keyword in js.  becouse of issu in block scope  and function scope 
+console.table([accountid , accemail,accno,city])
+console.log(typeof undefined)
+let num ="abc"
+let num1=Number(num)
+console.log(num1)
